11f5fb4bee1ad94768ab1c28425853f59064f5d4
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-07-24 23:14:21 +0900

    feat: 🎸 デフォルト表示ページの基本的データを取得できるようにした

--- FetchFukkanComments.ts
+++ FetchFukkanComments.ts
@@ -12,10 +12,39 @@ class FetchFukkanComments {
     const page = await browser.newPage()
     const baseUrl = 'https://www.fukkan.com/fk/VoteComment'
     // TODO: パラメータはオブジェクトで取り扱うようにする
+    // FIXME: pageパラメータの追加（コンストラクタにも）
     const params = `?no=${this.bookNo}&s=date`
 
     await page.goto(`${baseUrl}${params}`)
-    await page.screenshot({ path: 'tmp/screenshot.png' })
+
+    const comments: any = await page.locator('ul.comment_list li')
+    const numberOfComments = await comments.count()
+
+    type FukkanComment = {
+      userName: string
+      commentDetail: string
+    }
+    const fukkanComments: FukkanComment[] = []
+
+    for (let i = 0; i < numberOfComments; i++) {
+      const userName: string = await comments
+        .nth(i)
+        .locator('.comment_user p')
+        .innerText()
+      const commentDetail: string = await comments
+        .nth(i)
+        .locator('.comment_detail')
+        .innerText()
+
+      // TODO: commentDetail から年月日の抽出を行う
+      fukkanComments.push({
+        userName,
+        commentDetail,
+      })
+    }
+
+    console.log(fukkanComments)
+
     await browser.close()
   }
 }
